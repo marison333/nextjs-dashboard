0a21aa6c6410b672e3c5851a6573f550fcf9a0fc
Marison <marison.sol@outlook.com>
2024-09-20 13:28:10 +0200

    added pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,10 +10,9 @@ export default function Search({placeholder}: { placeholder: string }) {
     const { replace } = useRouter();
 
         const handleSearch = useDebouncedCallback((term) => {
-            console.log(`Searching... ${term}`);
-
         const params = new URLSearchParams(searchParams);
-        if (term) {
+            params.set('page', '1');
+            if (term) {
             params.set('query', term);
         } else {
             params.delete('query');
